89c6fd5ba38551285b4648be80bd940079ce6faf
Suele <suelen.ads.guimaraes@gmail.com>
2019-06-14 20:55:23 -0300

    Adiciona a funcionalidade que atribui likes a um post.

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -3,10 +3,13 @@ const router = new express.Router();
 const multer = require("multer");
 const uploadConfig = require("./config/upload");
 const PostController = require("./controllers/PostController");
+const LikeController = require("./controllers/LikeController");
 const upload = multer(uploadConfig);
 
 // passa a imagem pela rota, neste caso apenas uma imagem.
 router.post("/posts", upload.single("image"), PostController.store);
 router.get("/posts", PostController.index);
+// passa-se por parametro qual a imagem que deverá ter um like.
+router.post("/posts/:id/like", LikeController.store);
 
 module.exports = router;
