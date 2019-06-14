f057ea587b9efd5b07c0fced11ab7364e5f2856c
Suele <suelen.ads.guimaraes@gmail.com>
2019-06-14 17:23:35 -0300

    Adiciona a verificação de conexão com o banco de dados.

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -8,4 +8,11 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 
+// verifica se a conexão com o banco de dados foi realizada.
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error with database: "));
+db.once("open", () => {
+  console.log("mongoDB run");
+});
+
 app.listen(3333, () => console.log("server run port 3333"));
