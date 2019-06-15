477ca6849abc83c22c9259c7d25d3b77740a57fa
Suele <suelen.ads.guimaraes@gmail.com>
2019-06-15 00:51:52 -0300

    Altera o tamanho da imagem para uma melhor resolução no front-end e adiciona o monitoriamento de novos posts pelo socket.io.

--- back-end/src/controllers/PostController.js
+++ back-end/src/controllers/PostController.js
@@ -1,7 +1,12 @@
 const Post = require("../models/Post");
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = {
   async index(req, res) {
+    // busca todas as imagens e as ordena de acordo com o -createdAt,
+    // ou seja pelo ultimo post que foi adiciona(criado).
     const posts = await Post.find().sort("-createdAt");
     res.json(posts);
   },
@@ -10,16 +15,34 @@ module.exports = {
     console.log("req.file: ", req.file);
     console.log("req.body: ", req.body);
 
+    // desestrutura o objeto.
     const { author, place, description, hashtags } = req.body;
     const { filename: image } = req.file;
 
+    // adiciona a extenção jpg a imagem.
+    const [name] = image.split(".");
+    const fileName = `${name}.jpg`;
+
+    // manipula a imagem.
+    await sharp(req.file.path)
+    .resize(500)
+    .jpeg({quality: 70})
+    .toFile(
+      path.resolve(req.file.destination, "resized", fileName)
+    )
+
+    fs.unlinkSync(req.file.path);
+
+    // cria um post.
     const post = await Post.create({
       author,
       place,
       description,
       hashtags,
-      image
+      image: fileName
     });
+
+    req.io.emit("New post", post);
 
     return res.json(post);
   }
