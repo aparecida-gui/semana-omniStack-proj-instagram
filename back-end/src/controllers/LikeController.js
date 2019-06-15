ec6bd0c4670ea46ded613e20ccc9422ce7f61b07
Suele <suelen.ads.guimaraes@gmail.com>
2019-06-15 00:54:28 -0300

    Adiciona o monitoriamento de novos likes em tempo real pelo socket.io

--- back-end/src/controllers/LikeController.js
+++ back-end/src/controllers/LikeController.js
@@ -1,12 +1,14 @@
-const Post = require('../models/Post');
+const Post = require("../models/Post");
 
 module.exports = {
-  async store(req, res){
-
+  async store(req, res) {
+    // busca o post que tenha o id passado por parametro.
     const post = await Post.findById(req.params.id);
-    post.likes +=1;
+    post.likes += 1;
     await post.save();
+
+    req.io.emit("New like", post);
 
     return res.json(post);
   }
-}
+};
