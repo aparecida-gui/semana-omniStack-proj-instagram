00ce1f4c5bcc23b83684c7a4d400216db6c45d03
Suele <suelen.ads.guimaraes@gmail.com>
2019-06-19 15:42:03 -0300

    Adiciona a rota para inserir ate 3 imagens ao mesmo tempo.

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -4,6 +4,7 @@ const multer = require("multer");
 const uploadConfig = require("./config/upload");
 const PostController = require("./controllers/PostController");
 const LikeController = require("./controllers/LikeController");
+const ProfileController = require("./controllers/ProfileController");
 const upload = multer(uploadConfig);
 
 // passa a imagem pela rota, neste caso apenas uma imagem.
@@ -11,5 +12,8 @@ router.post("/posts", upload.single("image"), PostController.store);
 router.get("/posts", PostController.index);
 // passa-se por parametro qual o post que recebera um like.
 router.post("/posts/:id/like", LikeController.store);
+
+// rotas do perfil do usuário.
+router.post("/profile", upload.array("images", 3), ProfileController.store);
 
 module.exports = router;
